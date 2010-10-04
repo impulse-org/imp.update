995f469f9c80736fa5825a7df41361fcc47bd5aa
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-10-04 21:25:58 +0000

    Now reads the feature's plugin project list from the feature manifest, if no explicit list of projects is given. Also makes a few attempts to locate the feature manifest in several places relative to the current directory.

--- scripts/tag-feature.sh
+++ scripts/tag-feature.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-usageMsg="Usage: $0 [--test] feature qualifier project1 project2 ..."
+#
+# A simple script to SVN-tag a set of projects in the given feature with the given qualifier.
+# If no projects are specified, all projects (including the feature project itself) are tagged.
+#
+
+usageMsg="Usage: $0 [--help] [--test] feature qualifier [project1 project2 ...]"
 
 baseURL=https://dev.eclipse.org/svnroot/technology/org.eclipse.imp
+
+if [[ "$1" == "--help" ]]; then
+    echo "$usageMsg"
+    exit 0
+fi
 
 if [ "$1" == "--test" ]; then
     echo=echo
     shift
 fi
 
-if [ $# -lt 3 ]; then
+if [ $# -lt 2 ]; then
     echo "$usageMsg"
     exit 1
 fi
@@ -17,15 +27,43 @@ fi
 feature=$1; shift
 qualifier=$1; shift
 
-version=$(grep version ../$feature.feature/feature.xml | head -2 | tail -1 | sed -e 's/ *version="\(.*\).qualifier"/\1/')
+featureXMLSuffix=$feature.feature/feature.xml
 
-echo "-----------------------------------"
+featureXML=../../$featureXMLSuffix # Valid if we start in the same directory as this script
+
+if [[ ! -r $featureXML ]]; then
+    featureXML=../$featureXMLSuffix # Valid if we start in org.eclipse.imp.update
+fi
+
+if [[ ! -r $featureXML ]]; then
+    featureXML=./$featureXMLSuffix # Valid if we start at the workspace root
+fi
+
+if [[ ! -r $featureXML ]]; then
+    echo "Feature manifest $featureXML not found."
+    exit 1
+fi
+
+version=$(grep version $featureXML | head -2 | tail -1 | sed -e 's/ *version="\(.*\).qualifier"/\1/')
+
+if [[ $# -gt 0 ]]; then
+    projects=$*
+else
+    # Read the list of contained projects from the feature manifest.
+    # The first sed invocation that follows extracts lines containing '<plugin' followed by the
+    # subsequent line that invariably reads 'id="..."'. The second sed invocation just strips
+    # the plugin ID from the second line.
+    projects=$(sed -n -e '/<plugin$/,/id="/p' < $featureXML | sed -E -n -e 's/ *id="(.+)"/\1/p' | tr '\n' ' ')
+    projects="$projects $feature.feature"
+fi
+
+echo "-----------------------------------------------"
+echo "--- Feature ID is $feature ---"
 echo "--- Feature version is $version ---"
-echo "-----------------------------------"
+echo "--- Feature projects: $projects ---"
+echo "-----------------------------------------------"
 
-while [ $# -gt 0 ]; do
-    project=$1; shift
-
-    echo "Tagging $project..."
+for project in $projects; do
+    echo "--- Tagging $project..."
     $echo svn copy --parents -m 'Tagged for new feature release' $baseURL/trunk/$project $baseURL/tags/features/$feature/$version.$qualifier/$project
 done
